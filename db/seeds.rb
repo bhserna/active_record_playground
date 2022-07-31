e4839b3717ec5ae1e2124b444a6edc7308639739
bhserna <bhserna@gmail.com>
2022-07-31 07:51:29 -0500

    Better seeds example

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,23 @@
-require_relative "config"
-require_relative "../lib/models"
+# require "ffaker"
+# require_relative "config"
+# require_relative "../lib/models"
 
-# Here you can create your seeds here using your models
-#
-# Comment.create(body: "My comment")
+# def create_list(klass, count:, &block)
+#   records_data = count.times.map(&block)
+#   ids = klass.insert_all(records_data).map { |data| data["id"] }
+#   klass.where(id: ids)
+# end
+
+# def create_list_for_each_record(klass, records:, count:, &block)
+#   new_records_data = records.flat_map { |record| count.times.map { block.(record) } }
+#   ids = klass.insert_all(new_records_data).map { |data| data["id"] }
+#   klass.where(id: ids)
+# end
+
+# posts = create_list(Post, count: 10) do
+#   { title: FFaker::CheesyLingo.title, body: FFaker::CheesyLingo.paragraph }
+# end
+
+# create_list_for_each_record(Comment, records: posts, count: 20) do |post|
+#   { post_id: post.id, body: FFaker::CheesyLingo.sentence }
+# end
